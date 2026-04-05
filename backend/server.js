78dd3333c54877eb9819f2f21eaaa457fcb3469d
Pruthvi Sankhe <sankhepiu@gmail.com>
2026-04-05 20:57:47 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/submit", (req, res) => {
+  const { stress } = req.body;
+
+  let result = "Normal";
+
+  if (stress > 3) result = "High Stress";
+
+  res.json({ result });
+});
+
+app.listen(3000, () => console.log("Server running"));
